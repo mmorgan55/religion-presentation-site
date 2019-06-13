f161be48ac733f4809a9433524fa92c85900f360
Matthew Morgan <mmorgan55@cnm.edu>
2019-06-13 17:52:28 -0600

    Added starter code for the app

--- religion_flutter_app/lib/main.dart
+++ religion_flutter_app/lib/main.dart
@@ -1,1 +1,26 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(LearnReligionApp());
+
+class LearnReligionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "TBD",
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  AppState createState() => AppState();
+}
+
+class AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
