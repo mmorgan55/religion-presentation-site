962331447496d6a712a171fa4210afae9894d990
Matthew Morgan <mmorgan55@cnm.edu>
2019-06-18 23:23:52 -0600

    Added Flexibles for testing

--- religion_flutter_app/lib/main.dart
+++ religion_flutter_app/lib/main.dart
@@ -8,7 +8,7 @@ class LearnReligionApp extends StatelessWidget {
     return MaterialApp(
       title: "TBD",
       home: Scaffold(
-        body: Container(),
+        body: App(),
       ),
     );
   }
@@ -21,6 +21,62 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Container(
+      padding: EdgeInsets.all(25),
+      child: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 3,
+            fit: FlexFit.tight,
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 10.0,
+                    offset: Offset(0, 10),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  "TEST",
+                  style: TextStyle(
+                    fontFamily: "Georgia",
+                    fontSize: 50,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            fit: FlexFit.tight,
+            child: Container(),
+          ),
+          Flexible(
+            flex: 3,
+            fit: FlexFit.tight,
+            child: Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 10.0,
+                    offset: Offset(0, 10),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
